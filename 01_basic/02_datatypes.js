b39ad5a1c68ddeb1d30e46c779bef6b82174bceb
Rahul Raj <154469024+RAHUL-CB25@users.noreply.github.com>
2026-07-14 11:10:01 +0000

    data types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer verson
+
+// alert(3 + 3)  // we are  using node js, not browser
+
+
+let name="Rahul"
+let age=18
+let isLoggedIn=false
+
+
+//primitive data type
+//number =>  2 to power 53
+//bigint
+//String => ""
+//boolean => true, false
+//null=> stand alone value (jo ki empty hai khali...) and data types in java
+//undefined =>  value doesnot defined yrt
+//symbol => helps to find uniqueness
+
+
+//Object
+
+
+console.log(typeof "Rahul" );
+console.log(typeof age );
+console.log(typeof null );   //Object
+console.log(typeof undefined ); //undefined
+
+
